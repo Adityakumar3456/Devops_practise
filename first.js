aaf4e4c1c3f56aed0fa6c5063967ae84972d83f1
Aditya Kumar <ap704257@gmail.com>
2026-02-26 20:04:26 +0530

    First Commit

---
+++ first.js
@@ -0,0 +1,12 @@
+Aditya = "Hello"
+console.log(Aditya);
+age = 21;
+console.log(age);
+
+//template literlas
+
+let a = 5;
+let b = 20;
+
+var output =  `This is the total of a & b ${a + b}`; //`  `= backtick
+console.log(output);
---
+++ method.js
@@ -0,0 +1,2 @@
+var abc = " Hello ";
+abc.trim();
